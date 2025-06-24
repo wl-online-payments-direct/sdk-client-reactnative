ff7997d20e6c002d46f6b1b567942111f099fc1b
Worldline Direct support team <82139942+worldline-direct-support-team@users.noreply.github.com>
2025-06-24 11:33:45 +0200

    Release 1.1.3

--- src/api/session.tsx
+++ src/api/session.tsx
@@ -48,7 +48,7 @@ export class Session {
   private isEnvironmentProduction: boolean;
   private appIdentifier: string;
   private loggingEnabled: boolean;
-  private sdkIdentifier = 'ReactNativeClientSDK/v1.1.2';
+  private sdkIdentifier = 'ReactNativeClientSDK/v1.1.3';
 
   constructor(sessionDetails: {
     clientSessionId: string;
